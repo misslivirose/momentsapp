8496bd6b4e80a65f37c2fb76adfdbade996e6081
Liv <livi.erickson@gmail.com>
2017-10-23 23:17:47 -0700

    get post data from text input

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 var express = require('express');
 var path = require('path');
 var azure = require('azure-storage');
+var bodyParser = require('body-parser');
 require('dotenv').config();
 
 var app = express();
@@ -9,12 +10,20 @@ var _port = process.env.PORT || 1337;
 
 app.set('port', _port);
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({  
+    extended: true
+}));
 
-console.log(process.env.AZURE_STORAGE_ACCESS_KEY);
 
 var server = app.listen(app.get('port'), function () {
     var port = server.address().port;
- 
+});
+
+app.post('/send', function (req, res) {
+    var text = req.body.descriptor;
+    var rating = req.body.happiness;
+    
 });
 
 
