1d99f8e2ea2a57db5d5adfb82e4beb69bea62287
Liv <livi.erickson@gmail.com>
2017-10-29 17:01:52 -0700

    add skeleton placeholder for ac script

---
+++ hifi-content/moment-manager-ac.js
@@ -0,0 +1,23 @@
+﻿//
+// Moment Manager Assignment Client Script
+// Run on domain server to check periodically for new moments
+// 
+var REQUEST_URL = process.env.AZURE_FUNCTIONS_URL;
+var ORIGIN_POINT = { x: 0, y: -10, z: 0 };
+var UPDATE_TIMES = { 0:00, 6:00, 12:00, 18:00 } // Check for updates 4x per day
+
+var MOMENTS_BASE_PROPERTIES = {
+    type: "text"
+};
+
+function getNumberOfMoments() {
+    // TODO: Not yet implemented
+}
+
+function findPlacementPosition() {
+    // TODO: Not yet implemented
+    // Starting from the origin point, calculate the number of existing moments and remaining ones to add
+    // When a circle is full, create a new concentric circle around the existing moments and structure new moments there
+}
+
+
